b95d05a7009f899f324c93a5253bd1b5d56f20d6
rachana-k-kulkarni <rachana.k.kulkarni@gmail.com>
2024-07-25 23:54:39 +0530

    add 23

---
+++ C Programming/Level4 Problems/Problem_L4_23.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int sum =0,prime;
+    for(int i=2;i<10;i++){
+        prime = 1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                prime=0;
+                break;
+            }
+        }
+        if(prime)
+        sum = sum+i;
+    }
+    printf("%d",sum);
+}
