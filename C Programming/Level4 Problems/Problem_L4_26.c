9f926fd54bf4ab30b56eaa83f0bd35b84b1d1c04
rachana-k-kulkarni <rachana.k.kulkarni@gmail.com>
2024-07-26 23:16:02 +0530

    add 26

---
+++ C Programming/Level4 Problems/Problem_L4_26.c
@@ -0,0 +1,17 @@
+//not optimised
+#include<stdio.h>
+int main(){
+    int sum =0,prime;
+    for(int i=1000;i>100;i--){
+        prime = 1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                prime=0;
+                break;
+            }
+        }
+        if(prime)
+        sum = i;
+    }
+    printf("%d",sum);
+}
